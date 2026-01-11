04a21b847bcd278ba97ae43b591249b6926b585e
Olivia Jakli <jakli.olivia@gmail.com>
2026-01-10 21:01:28 -0500

    removed old config file. new one added to utils folder

--- src/config_loader.py
+++
@@ -1,7 +0,0 @@
-import yaml
-from pathlib import Path
-
-def load_config(config_path="config/config.yaml"):
-    """Load configuration from a YAML file."""
-    with open(Path(config_path), 'r') as file:
-        return yaml.safe_load(file)
