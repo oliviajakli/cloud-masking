4276275ab936dd1c058683d33b482096ed87dd30
Olivia Jakli <jakli.olivia@gmail.com>
2026-01-23 00:53:01 -0500

    file name change and restructuring

--- analysis/run_exploratory.py
+++ analysis/run_exploratory.py
@@ -1,15 +1,15 @@
 from src.descriptive_stats import compute_descriptive_stats
-from src.exploratory_plots import plot_distributions, plot_boxplots_with_stats, \
-bootstrap_ci, plot_paired_differences, plot_bland_altman, plot_error_maps, \
-plot_scatterplot, plot_time_series
+from src.exploratory_plots import (plot_distributions, plot_boxplots_with_stats,
+    plot_paired_differences, plot_bland_altman, plot_error_maps,plot_scatterplot, 
+    plot_time_series)
 from src.utils.config import load_config
 from pathlib import Path
 
-import pandas as pd
+import pandas as pd   # type: ignore
 
 config = load_config()
 
-input_data = "data/per_scene_evaluation_metrics_20260116_171319.csv"
+input_data = Path(config["paths"]["input"])
 metrics = config["metrics"]
 pairs = config["algorithm_pairs"]
 algorithms = config["algorithms"]
@@ -17,9 +17,7 @@ samples = config["samples"]
 reference_masks = Path(config["paths"]["reference_masks_dir"])
 output_dir = Path(config["paths"]["output_dir"])
 
-df = pd.read_csv(input_data)
-
-def main():
+def main(df):
     compute_descriptive_stats(df, output_dir)
     plot_distributions(df, metrics, output_dir)
     plot_boxplots_with_stats(df, metrics, pairs, algorithms, f"{output_dir}/boxplots")
@@ -31,4 +29,6 @@ def main():
     return "Descriptive analysis and plots completed. Results saved to:", output_dir
 
 if __name__ == "__main__":
-    print(main())
+    df = pd.read_csv(input_data)
+    message, path = main(df)
+    print(message, path)
