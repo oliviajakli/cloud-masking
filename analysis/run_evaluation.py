b62e61ba612e3530e4bef79cf8caa9292a01e2f6
Olivia Jakli <jakli.olivia@gmail.com>
2026-01-27 00:17:16 -0500

    logging metric evaluation process, docstring, type hint

--- analysis/run_evaluation.py
+++ analysis/run_evaluation.py
@@ -3,7 +3,10 @@ from src.utils.config import load_config
 from src.utils.io import save_csv
 from pathlib import Path
 import os
+import logging
 from sklearn.metrics import confusion_matrix    # type: ignore
+
+logger = logging.getLogger(__name__)
 
 config = load_config()
 
@@ -11,15 +14,22 @@ algorithms = config["algorithms"]
 masks_dir = Path(config["paths"]["data_root"]) / "masks"
 reference_masks = Path(config["paths"]["reference_masks_dir"])
 
-def main():
-    # Run evaluation metrics, plot confusion matrices, and save results.
+def main() -> tuple[str, str]:
+    """Run evaluation metrics, plot confusion matrices, and save results.
+    Returns:
+        message: str, status message
+        output_csv: str, path to saved CSV file with evaluation metrics
+    """
+    logger.info("Starting evaluation process...")
     df = compute_metrics(masks_dir)
     reference_masks_list = load_masks(reference_masks)
+    logger.debug(f"Loaded {len(reference_masks_list)} reference masks for evaluation.")
     for alg in algorithms:
         alg_masks_list = load_masks(Path(config["paths"][f"{alg}_masks_dir"]))
         for i, (ref_mask, alg_mask) in enumerate(zip(reference_masks_list, alg_masks_list)):
             cm = confusion_matrix(ref_mask, alg_mask)
             plot_confusion_matrix(cm, title=f"{alg} Confusion Matrix Scene {i+1}")
+            logger.info(f"Plotted confusion matrix for algorithm '{alg}', scene {i+1}")
     # Save evaluation metrics to CSV in the data directory to use for analysis.
     output_csv = os.path.join('data', 'per_scene_evaluation_metrics.csv')
     save_csv(df, Path(output_csv))
